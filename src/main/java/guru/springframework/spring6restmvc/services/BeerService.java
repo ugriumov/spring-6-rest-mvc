100056104dace0a3743765204a62d4e179e11ed2
Sergei Ugriumov <unkas.mailbox@gmail.com>
2023-05-08 18:41:14 +0200

    all_lombok_updates

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+
+import java.util.UUID;
+
+public interface BeerService {
+
+    Beer getBeerById(UUID id);
+
+}
---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -0,0 +1,32 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer Id in service was called");
+
+        return Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+    }
+}
